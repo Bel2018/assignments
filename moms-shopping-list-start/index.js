79ee58a7513529ff31c2e5b4a1efe197b88f57dd
Bel Reh <behrehhtoo@gmail.com>
2020-05-25 11:06:11 -0600

    add delete button and clear

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,29 +1,74 @@
 
 const ulList = document.getElementById('list');
 const listInput = document.getElementById('title');
-const form = document.getElementById('add-todo').addEventListener('submit', addLists);
+const form = document.getElementById('add-todo');
+const clear = document.querySelector('.clear-btn');
+// const deleteButton = document.querySelector('.delete-btn');
 
+
+ // load eventlistener
+form.addEventListener('submit', addLists);
+
+
+
+// clear list button
+clear.addEventListener('click', clearLists);
+
+
+// create function
 function addLists(e) {
+    if(listInput.value==='') {
+      alert('Please add your list');
+    }
+    // create li
+    const li = document.createElement('li');
 
-// create li
-const li = document.createElement('li');
-// create div
-const div = document.createElement('div');
-
-const buttonOne = document.createElement('button');
-const buttonTwo = document.createElement('button');
-
-
-div.appendChild(document.createTextNode(listInput.value));
-buttonOne.appendChild(document.createTextNode('edit'));
-buttonTwo.appendChild(document.createTextNode('X'));
-
-li.appendChild(div);
-li.appendChild(buttonOne);
-li.appendChild(buttonTwo);
-ulList.appendChild(li);
-listInput.value = '';
+    // crete li class
+    li.className='list-item';
 
 
-  e.preventDefault();
+    // create div
+    const div = document.createElement('div');
+    // add className
+    div.className = 'text-list';
+
+    // crete edit button
+    const buttonOne = document.createElement('button');
+    // add class
+    buttonOne.className = 'edit-btn';
+
+    const buttonTwo = document.createElement('button');
+    buttonTwo.className = 'delete-btn';
+
+
+    div.appendChild(document.createTextNode(listInput.value));
+    buttonOne.appendChild(document.createTextNode('edit'));
+    buttonTwo.appendChild(document.createTextNode('X'));
+
+    buttonTwo.addEventListener('click', deleteList);
+
+    li.appendChild(div);
+    li.appendChild(buttonOne);
+    li.appendChild(buttonTwo);
+    ulList.appendChild(li);
+    listInput.value = '';
+
+    console.log(li);
+
+      e.preventDefault();
+}
+
+// delete button
+function deleteList(e) {
+
+    e.target.parentElement.remove();
+
+}
+
+
+// clear lists button
+function clearLists() {
+  while(ulList.firstChild){
+    ulList.removeChild(ulList.firstChild);
+  }
 }
