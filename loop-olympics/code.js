301404b0f1a369a38e3917df81271ef16089cc69
Bel Reh <behrehhtoo@gmail.com>
2020-05-26 13:07:07 -0600

    add loop code

--- loop-olympics/code.js
+++ loop-olympics/code.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+// for loop throug number 0-10
+
+for(let i=0; i < 10; i++){
+  console.log(i);
+}
+
+for(let i=9; i > -1; i--){
+  console.log(i);
+}
+
+var fruit = ["banana", "orange", "apple", "kiwi"]
+for( let i=0; i < fruit.length; i++){
+  console.log(fruit[i]);
+}
